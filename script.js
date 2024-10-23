a3ea9f128ddc06e3068da326ed1f1c4b35404c9c
WillingtonA <williesdescomunal@gmail.com>
2024-10-22 19:34:33 -0500

    Actualizado

--- script.js
+++ script.js
@@ -1,112 +1,97 @@
-function dgebi(id){
-	return document.getElementById(id).value;
-}
 
-function dsebi(id, value){
-	document.getElementById(id).value = value;
-}
-
-function number(num){
-	var numUno = dgebi("numUno");
-	if(numUno == "0"){ //Primera vez numero
-		dsebi("numUno", num);
-		dsebi("pantalla", new Intl.NumberFormat('de-DE').format(num));
-	}else{
-		dsebi("numUno", numUno+num);
-		dsebi("pantalla", new Intl.NumberFormat('de-DE').format(numUno+num));	
+function dgebiSetter(obj) {
+	/*
+	console.log(obj);
+	console.log(Object.keys(obj));
+	console.log(Object.values(obj));
+	console.log("--" + Object.keys(obj).length);
+	*/
+	for(let key in obj) {
+		document.getElementById(key).value = obj[key];
 	}
 }
 
-function operation(operador){
-	var numUno = dgebi("numUno");
-	var numDos = dgebi("numDos");
-	var signo = dgebi("operador");
-	
-	if(numDos == "0"){ //Primera vez signo
-		dsebi("numDos", numUno);
-		dsebi("numUno", "0");
-	}else{
-		var res = respuesta(signo, numUno, numDos);
-		dsebi("pantalla", new Intl.NumberFormat('de-DE').format(res));
-		dsebi("numUno", "0");
-		dsebi("numDos", res);
-	}
-	dsebi("operador", operador);
-	dsebi("punto", "");
+function dgebiGetter(keys) {
+	let obj = new Object();
+	keys.forEach(
+		function(key){
+			obj[key] = document.getElementById(key).value;
+		}
+	);
+	return obj;
 }
 
-function igual(){
-	var numUno = dgebi("numUno");
-	var numDos = dgebi("numDos");
-	var signo = dgebi("operador");
-
-	if(signo != ""){
-		var res = respuesta(signo, numUno, numDos);
-		dsebi("pantalla", new Intl.NumberFormat('de-DE').format(res));
-		dsebi("numUno", res);
-		dsebi("numDos", "0");
-		dsebi("operador", "");
+function number(num) {
+	let numUno = document.getElementById("numUno").value;
+	if(numUno == "0") { //Primera vez numero
+		dgebiSetter({'numUno':num, 'pantalla':new Intl.NumberFormat('de-DE').format(num)});
+	} else {
+		dgebiSetter({"numUno":numUno + num, "pantalla":new Intl.NumberFormat('de-DE').format(numUno + num)});
 	}
 }
 
-function punto(){
-	var numUno = dgebi("numUno");
-	var punto = dgebi("punto");
-	dsebi("punto", ".");
+function operation(operator) {
+	let data = dgebiGetter(["numUno", "numDos", "operador"]);
+	if(data.numDos == "0") { //Primera vez signo
+		dgebiSetter({"numDos":data.numUno, "numUno":"0"});
+	} else {
+		let res = respuesta(data.operador, data.numUno, data.numDos);
+		dgebiSetter({"pantalla":new Intl.NumberFormat('de-DE').format(res), "numUno":"0", "numDos":res});
+	}
+	dgebiSetter({"operador":operator, "dot":""});
+}
 
-	if(numUno == "0"){
+function equal() {
+	let data = dgebiGetter(["numUno", "numDos", "operador"]);
+	if(data.operador != "") {
+		let res = respuesta(data.operador, data.numUno, data.numDos);
+		dgebiSetter({"pantalla":new Intl.NumberFormat('de-DE').format(res), "numUno":res, "numDos":"0", "operador":""});
+	}
+}
+
+function dot() {
+	let data = dgebiGetter(["numUno", "dot"]);
+	document.getElementById("dot").value = ".";
+
+	if(data.numUno == "0") {
 		number( "0.");
-	}else{
-		if(punto != "."){
+	} else {
+		if(data.dot != ".") {
 			number(".");
 		}
 	}
 }
 
-function ce(){
-	dsebi("pantalla", "0");
-	dsebi("numUno", "0");
-	dsebi("punto", "");
-}
+function ce() {
+	let numberOne = document.getElementById("numUno").value;
+	let numberShort = "";
 
-function c(){
-	dsebi("pantalla", "0");
-	dsebi("numUno", "0");
-	dsebi("numDos", "0");
-	dsebi("operador", "");
-	dsebi("punto", "");
-}
-
-function respuesta(operador, numUno, numDos){
-	if(operador == "+"){
-		return suma(numUno, numDos);
+	for(let i=0; i<numberOne.length-1; i++) {
+		numberShort += numberOne.charAt(i);
 	}
 
-	if(operador == "-"){
-		return resta(numDos, numUno);
+	document.getElementById("numUno").value = "0";
+	number(numberShort);
+}
+
+function clean() {
+	dgebiSetter({"pantalla":"0", "numUno":"0", "numDos":"0", "operador":"", "dot":""});
+}
+
+function respuesta(operator, numUno, numDos) {
+	if(operator == "+") {
+		return parseFloat(numUno) + parseFloat(numDos);
 	}
 
-	if(operador == "*"){
-		return multi(numDos, numUno);
+	if(operator == "-") {
+		return parseFloat(numDos) - parseFloat(numUno);
 	}
 
-	if(operador == "/"){
-		return div(numDos, numUno);
+	if(operator == "*") {
+		return parseFloat(numDos) * parseFloat(numUno);
 	}
-}
 
-function suma(numUno, numDos){
-	return parseFloat(numUno) + parseFloat(numDos);
-}
-
-function resta(numUno, numDos){
-	return parseFloat(numUno) - parseFloat(numDos);
-}
-
-function multi(numUno, numDos){
-	return parseFloat(numUno) * parseFloat(numDos);
-}
-
-function div(numUno, numDos){
-	return parseFloat(numUno) / parseFloat(numDos);
+	if(operator == "/") {
+		return parseFloat(numDos) / parseFloat(numUno);
+	}
 }
